2cf68c91160d65b982fa6cd11319ad659f8258b4
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2023-05-18 19:17:35 -0500

    Se genera enviroment para coneccion con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import {
+  AngularFirestoreModule,
+  SETTINGS,
+  Settings,
+} from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firestore, 'cine-distrito'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
