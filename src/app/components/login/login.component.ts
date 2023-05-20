6782718b8bb041f4240e49611d8ac91c32c2c554
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2023-05-19 23:21:10 -0500

    implmentacion de logout y funcionalidad de sigin

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private loginService: LoginService) {}
+  ngOnInit(): void {
+    this.loginService.getAuth().subscribe((auth) => {
+      if (auth) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+  login() {
+    this.loginService
+      .login(this.email, this.password)
+      .then((res) => {
+        console.log('usuario logueado');
+        this.router.navigate(['/']);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router, private loginService: LoginService) {}
+  ngOnInit(): void {
+    this.loginService.getAuth().subscribe((auth) => {
+      if (auth) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+  registro() {
+    this.loginService
+      .registrarse(this.email, this.password)
+      .then((res) => {
+        this.router.navigate(['/']);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
