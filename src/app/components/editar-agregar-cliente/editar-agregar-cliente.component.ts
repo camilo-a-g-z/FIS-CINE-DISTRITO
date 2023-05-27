2191a78423519a240d3661d762d4019aa5d199bc
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2023-05-26 21:42:56 -0500

    se trabaja con formulario de empleado

--- src/app/components/editar-agregar-cliente/editar-agregar-cliente.component.ts
+++ src/app/components/editar-agregar-cliente/editar-agregar-cliente.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Empleado } from 'src/app/modelo/empleado.model';
+import { EmpleadoService } from 'src/app/servicios/empleado.service';
 
 @Component({
   selector: 'app-editar-agregar-cliente',
@@ -8,13 +10,47 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditarAgregarClienteComponent implements OnInit {
   id: string;
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  isEdit: boolean;
+  empleado: null | Empleado = {
+    apellido: '',
+    cedula: '',
+    nombre: '',
+    salario: 0,
+    multiplex: '',
+    numTelefono: '',
+    fechaContrato: null,
+    correo: '',
+  };
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private empleadoService: EmpleadoService
+  ) {}
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     if (this.id == '0') {
       //estamos agregando un nuevo empleado
+      this.isEdit = false;
     } else {
       //estamos editando un empleado
+      this.isEdit = true;
+      this.empleadoService.getEmpleado(this.id).subscribe((empleado) => {
+        this.empleado = empleado;
+      });
+    }
+  }
+  guardar({ value, valid }: { value: Empleado; valid: boolean | null }) {
+    if (valid) {
+      if (this.isEdit) {
+        //modificar
+        this.empleadoService.modificarEmpleado(value);
+      } else {
+        //agregar
+        this.empleadoService.agregarEmpleado(value);
+      }
+      this.router.navigate(['/admin/empleados']);
+    } else {
+      console.log('Formulario no valido');
     }
   }
 }
