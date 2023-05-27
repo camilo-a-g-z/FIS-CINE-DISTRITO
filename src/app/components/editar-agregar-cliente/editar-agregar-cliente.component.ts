e06ce4129995d076926d6c19a2c1878aab5fccfd
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2023-05-26 19:16:16 -0500

    se emepieza a trabajar en agregar ye ditar empleado

---
+++ src/app/components/editar-agregar-cliente/editar-agregar-cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-agregar-cliente',
+  templateUrl: './editar-agregar-cliente.component.html',
+  styleUrls: ['./editar-agregar-cliente.component.css']
+})
+export class EditarAgregarClienteComponent {
+
+}
