2b73587dbba41e91d140f2d1ad92ddf130e00ee6
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2023-05-30 11:56:29 -0500

    se conecta servicio de salas

--- src/app/components/multiplex-admin/multiplex-admin.component.ts
+++ src/app/components/multiplex-admin/multiplex-admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Multiplex } from 'src/app/modelo/multiplex.model';
+import { Sala } from 'src/app/modelo/sala.model';
 import { MultiplexService } from 'src/app/servicios/multiplex.service';
+import { SalasService } from 'src/app/servicios/salas.service';
 
 @Component({
   selector: 'app-multiplex-admin',
@@ -10,13 +12,30 @@ import { MultiplexService } from 'src/app/servicios/multiplex.service';
 export class MultiplexAdminComponent implements OnInit {
   multiplexes: Multiplex[];
   multiplex: Multiplex;
-  constructor(private multiplexService: MultiplexService) {}
+  salas: Sala[];
+  sala: Sala = {
+    numero: 0,
+    sillas: [],
+  };
+  constructor(
+    private multiplexService: MultiplexService,
+    private salasService: SalasService
+  ) {}
   ngOnInit(): void {
     this.multiplexService.getMultiplexes().subscribe((multiplexes) => {
       this.multiplexes = multiplexes;
     });
+    //this.salas.push(this.sala);
   }
-  mostrar() {
-    console.log('Hola');
+  mostrarSalas(multiplex: Multiplex) {
+    //se limpia el array de salas
+    this.salas = [];
+    this.salasService.getSalasMultiplex(multiplex.nombre).subscribe((salas) => {
+      if (salas != null) {
+        this.salas = salas;
+      } else {
+        this.salas = [];
+      }
+    });
   }
 }
--- src/app/servicios/salas.service.ts
+++ src/app/servicios/salas.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Sala } from '../modelo/sala.model';
-import { Observable,map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { MultiplexService } from './multiplex.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SalasService {
+  constructor(
+    private db: AngularFirestore,
+    private mService: MultiplexService
+  ) {}
 
-  constructor(private db: AngularFirestore,private mService:MultiplexService) { }
-
-
-  getSalasMultiplex(multiplex:string): Observable<Sala[]>|null{
+  getSalasMultiplex(multiplex: string): Observable<Sala[]> {
     // Obtenemos las peliculas
-    if(this.mService.existeMultiplex(multiplex)){
-      let colec = this.db.collection("multiplex").doc(multiplex).collection("sala")
+    if (this.mService.existeMultiplex(multiplex)) {
+      let colec = this.db
+        .collection('multiplex')
+        .doc(multiplex)
+        .collection('sala');
       let salas = colec.snapshotChanges().pipe(
         map((cambios) => {
           return cambios.map((accion) => {
@@ -25,20 +29,27 @@ export class SalasService {
         })
       );
       return salas;
-    }else{
-      return null;
+    } else {
+      //devolvemos un observable vacio
+      return new Observable<Sala[]>();
     }
   }
 
-  agregarSala(sala:Sala,multiplex:string) {
-    if(this.mService.existeMultiplex(multiplex)){
-      let colec = this.db.collection("multiplex").doc(multiplex).collection("sala")
+  agregarSala(sala: Sala, multiplex: string) {
+    if (this.mService.existeMultiplex(multiplex)) {
+      let colec = this.db
+        .collection('multiplex')
+        .doc(multiplex)
+        .collection('sala');
       colec.add(sala);
     }
   }
 
-  getSala(numero:number,multiplex:string) {
-    let doc = this.db.doc(`multiplex/${multiplex}`).collection("sala").doc(numero.toString());
+  getSala(numero: number, multiplex: string) {
+    let doc = this.db
+      .doc(`multiplex/${multiplex}`)
+      .collection('sala')
+      .doc(numero.toString());
     let sala = doc.snapshotChanges().pipe(
       map((accion) => {
         if (accion.payload.exists === false) {
@@ -52,13 +63,19 @@ export class SalasService {
     return sala;
   }
 
-  modificarSala(sala:Sala,multiplex:string) {
-    let doc = this.db.doc(`multiplex/${multiplex}`).collection("sala").doc(sala.numero.toString());
+  modificarSala(sala: Sala, multiplex: string) {
+    let doc = this.db
+      .doc(`multiplex/${multiplex}`)
+      .collection('sala')
+      .doc(sala.numero.toString());
     doc.update(sala);
   }
 
-  eliminarSala(sala:Sala,multiplex:string) {
-    let doc = this.db.doc(`multiplex/${multiplex}`).collection("sala").doc(sala.numero.toString());
+  eliminarSala(sala: Sala, multiplex: string) {
+    let doc = this.db
+      .doc(`multiplex/${multiplex}`)
+      .collection('sala')
+      .doc(sala.numero.toString());
     doc.delete();
   }
 }
