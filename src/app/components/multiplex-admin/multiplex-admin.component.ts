d725cb2dabbc20afe9184bb53cda6868267613ab
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2023-05-29 21:57:51 -0500

    se inicia funcionalidad de multiplex desde admin

--- src/app/components/multiplex-admin/multiplex-admin.component.ts
+++ src/app/components/multiplex-admin/multiplex-admin.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Multiplex } from 'src/app/modelo/multiplex.model';
+import { MultiplexService } from 'src/app/servicios/multiplex.service';
 
 @Component({
   selector: 'app-multiplex-admin',
   templateUrl: './multiplex-admin.component.html',
-  styleUrls: ['./multiplex-admin.component.css']
+  styleUrls: ['./multiplex-admin.component.css'],
 })
-export class MultiplexAdminComponent {
-
+export class MultiplexAdminComponent implements OnInit {
+  multiplexes: Multiplex[];
+  multiplex: Multiplex;
+  constructor(private multiplexService: MultiplexService) {}
+  ngOnInit(): void {
+    this.multiplexService.getMultiplexes().subscribe((multiplexes) => {
+      this.multiplexes = multiplexes;
+    });
+  }
+  mostrar() {
+    console.log('Hola');
+  }
 }
