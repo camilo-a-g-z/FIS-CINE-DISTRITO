579d02e67831f00f6df425bd12ed976a7ef6e318
Laura Riobueno <133523053+LauwuRi@users.noreply.github.com>
2023-06-01 17:01:00 -0500

    resumen pago

---
+++ src/app/components/resumen-pago/resumen-pago.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resumen-pago',
+  templateUrl: './resumen-pago.component.html',
+  styleUrls: ['./resumen-pago.component.css']
+})
+export class ResumenPagoComponent {
+
+}
