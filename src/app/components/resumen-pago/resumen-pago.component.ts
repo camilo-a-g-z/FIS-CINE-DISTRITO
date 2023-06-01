30595bb478616541746c313a9f4ebde294a4b0b7
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-06-01 18:37:44 -0500

    creacion resumen de pago

--- src/app/components/resumen-pago/resumen-pago.component.ts
+++ src/app/components/resumen-pago/resumen-pago.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Compra } from 'src/app/modelo/compra.model';
+import { CompraService } from 'src/app/servicios/compra.service';
 
 @Component({
   selector: 'app-resumen-pago',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./resumen-pago.component.css']
 })
 export class ResumenPagoComponent {
+  compra:Compra
+  stringSillas:string = ""
+  cargado:boolean = false
+  constructor(private compraService:CompraService){}
 
+  ngAfterViewInit(){
+    console.log(this.compra)
+    this.compra = this.compraService.compra
+    for(let silla of this.compra.sillas){
+      this.stringSillas += silla + " "
+    }
+    this.cargado = true
+  }
 }
